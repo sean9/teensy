6bc24ad14bf8637bfd5a5595ef4a609528bdc7c5
sean9 <sean9@users.noreply.github.com>
2015-02-28 01:47:37 -0500

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -13,16 +13,16 @@ NO for no
     |-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|
     |   (   |   )   |   Z   |   X   |   C   |   V   |   B   |   N   |   M   |   ,   |   .   |  / ?  |
     |-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|
-    |   [   |   ]   |       |  esc  | shift |     space     |  rctl |  ralt |  FN0  |  FN1  |       |
+    |   [   |   ]   |  esc  |  del  | shift |     space     |  rctl |  ralt |  FN0  |  FN1  |       |
     `-----------------------------------------------------------------------------------------------'
     ,-----------------------------------------------------------------------------------------------.
     |   !   |   @   |   #   |   $   |   %   |   ^   |   &   |   *   |  -/_  |  =/+  |   \|  |   `~  |
     |-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|
-    | volup |  next |  left |  down |   up  | right |       |       |       | insert|  home | pageup|
+    |nxtsong|       |  left |  down |   up  | right |       |       |       | insert|  home | pageup|
     |-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|
-    |voldown|  prev |       |       |       |       |       |       |       |       |  end  | pagedn|
+    |presong|       |       |       |       |       |       |       |       |       |  end  | pagedn|
     |-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|
-    |volmute| pause |       |       |       |     space     |       |       |       |  win  |printsc|
+    |volmute| pause |  trns |  trns |       |     space     |  gui  |       |       |       |printsc|
     `-----------------------------------------------------------------------------------------------'
     ,-----------------------------------------------------------------------------------------------.
     |   7   |   8   |   9   | bkspc |       |       |       |       |   F1  |   F2  |   F3  |   F4  |
@@ -39,31 +39,30 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	BSPC,   QUOT,      Q,      W,      E,      R,      T,      Y,      U,      I,      O,      P, 
          TAB,   SCLN,      A,      S,      D,      F,      G,      H,      J,      K,      L,    ENT,
          FN3,    FN4,      Z,      X,      C,      V,      B,      N,      M,   COMM,    DOT,   SLSH,
-        LBRC,   RBRC,     NO,    ESC,   LSFT,            SPC,   RCTL,   RALT,    FN0,    FN1,    NO), 
+        LBRC,   RBRC,    ESC, DELETE,   LSFT,            SPC,   RCTL,   RALT,    FN0,    FN1,    NO), 
 
 	[1] = KEYMAP( 
 	 FN5,    FN6,    FN7,    FN8,    FN9,   FN10,   FN11,   FN12,   MINS,    EQL,   BSLS,    GRV,
-          NO,   MNXT,   LEFT,   DOWN,     UP,  RIGHT,     NO,     NO,     NO,    INS,   HOME,   PGUP,
-	  NO,   MPRV,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,    END, PGDOWN,
-	MUTE,   MPLY,     NO,     NO,     NO,            SPC,     NO,     NO,     NO,   LGUI,  PSCR),
+	MNXT,     NO,   LEFT,   DOWN,     UP,  RIGHT,     NO,     NO,     NO,    INS,   HOME,   PGUP,
+	MPRV,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,    END, PGDOWN,
+	MUTE,   MPLY,   TRNS,   TRNS,   TRNS,            SPC,    LGUI,  TRNS,   TRNS,   TRNS,  PSCR),
 
 	[2] = KEYMAP( 
            7,      8,      9,   BSPC,    NO,      NO,     NO,     NO,     F1,     F2,     F3,     F4,
            4,      5,      6,     NO,    NO,      NO,     NO,     NO,     F5,     F6,     F7,     F8,
            1,      2,      3,   PPLS,   PAST,     NO,     NO,     NO,     F9,    F10,    F11,    F12,
-           0,    DOT,    ENT,   PMNS,   PSLS,            SPC,     NO,     NO,     NO,     NO,    NO),
+           0,    DOT,    ENT,   PMNS,   PSLS,            SPC,   TRNS,     NO,   TRNS,   TRNS,  TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
 
 	[0] = ACTION_LAYER_MOMENTARY(1),               // first layer
-	[1] = ACTION_LAYER_MOMENTARY(2),               // lower Fn layer
-	[2] = ACTION_LAYER_MOMENTARY(3),               // raise Fn layer
+	[1] = ACTION_LAYER_MOMENTARY(2),               // second Fn layer
+	//[2] = ACTION_LAYER_TAP_KEY(3, KC_ESC),               // raise Fn layer
 
 	[3] = ACTION_MODS_KEY(MOD_LSFT, KC_9),         // (
 	[4] = ACTION_MODS_KEY(MOD_LSFT, KC_0),         // )
 	
-
 	[5] = ACTION_MODS_KEY(MOD_LSFT, KC_1),         // !
 	[6] = ACTION_MODS_KEY(MOD_LSFT, KC_2),         // @
 	[7] = ACTION_MODS_KEY(MOD_LSFT, KC_3),         // #
